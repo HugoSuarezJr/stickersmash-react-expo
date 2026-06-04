37323ac0af6cb8038e77d5b02037bbf5eb58e22b
Hugo Suarez <hugo.suarez92@yahoo.com>
2026-06-03 20:02:01 -0400

    first edits to app

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Welcome to your new app...</Text>
+      <Text>With Jehovah's help, this can lead to great things!</Text>
+      <Text>To serve him more fully 🙏</Text>
+      <Text>If it is His will...</Text>
+      <Text>I love Marisela Alcalá</Text>
+      <Text>I hope she is the one to be with me in this journey forever ❤️</Text>
+    </View>
+  );
+}
